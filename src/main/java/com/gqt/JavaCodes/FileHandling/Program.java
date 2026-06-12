69e3b88c99c3b35e307fdfa5690801a8ac9162d8
Moulika09-04 <satulurimoulika99@gmail.com>
2026-06-12 09:05:52 +0530

    File Handling programs

---
+++ src/main/java/com/gqt/JavaCodes/FileHandling/Program.java
@@ -0,0 +1,23 @@
+package com.gqt.JavaCodes.FileHandling;
+
+import java.util.ArrayList;
+
+public class Program {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		al.add(100);
+		al.add(50);
+		al.add(150);
+		al.add(25);
+		al.add(75);
+		al.add(125);
+		al.add(175);
+		System.out.println(al);
+		System.out.println("----------------");
+		al.stream().forEach(i->System.out.println(i));
+		System.out.println("-------------------");
+		al.stream().forEach(System.out::println);
+	}
+
+}
